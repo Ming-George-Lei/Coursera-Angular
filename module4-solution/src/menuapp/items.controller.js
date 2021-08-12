5dfa925ab92da65d51c92d63da7dcff86d1e50d9
Ming-George-Lei <58166637+Ming-George-Lei@users.noreply.github.com>
2021-08-12 20:52:37 +0800

    Add files via upload

---
+++ module4-solution/src/menuapp/items.controller.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('ItemsController', ItemsController);
+
+
+ItemsController.$inject = ['items'];
+function ItemsController(items) {
+  var menuItems = this;
+  menuItems.items = items;
+}
+
+})();
---
+++ module4-solution/src/menuapp/menudata.service.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('data')
+.service('MenuDataService', MenuDataService);
+
+
+MenuDataService.$inject = ['$http', '$q'];
+function MenuDataService($http) {
+  var service = this;
+
+  service.getAllCategories = function() {
+    return $http(
+      {
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/categories.json"
+      }
+    ).then(function (response) {
+      return response.data;
+    });
+  }
+
+  service.getItemsForCategory = function(categoryShortName) {
+    return $http(
+      {
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName
+      }
+    ).then(function (response) {
+      console.log(response.data.menu_items);
+      return response.data.menu_items;
+    });
+  }
+}
+
+})();
